3eca1553ce32568badb69d0c09356018255ae68d
Silvain <silver383@gmail.com>
2023-08-11 02:00:03 +0300

    news

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,19 +4,83 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 
+import { gsap } from "gsap";
+import { InertiaPlugin } from "gsap/InertiaPlugin.js";
+// import { ScrollToPlugin } from "gsap/ScrollToPlugin.js";
+// import { Draggable } from "gsap/Draggable.js";
+import { ScrollTrigger } from "gsap/ScrollTrigger.js"
+
+gsap.registerPlugin(ScrollTrigger);
+
+//----------------------------------------------------------------------------------------
+//анимация прогрес бара GSAP
+
+// анимация прогрес бара - 1-й вариант
+// const progressValueId = document.getElementById("progress-value-id")
+
+
+// gsap.fromTo(".circular-progress", { y: 0, background: "conic-gradient(#7d2ae8 0deg, #ededed 0deg)" }, {
+// 	y: 20, background: `conic-gradient(#7d2ae8 ${95 * 3.6}deg, #ededed 0deg)`, duration: 2,
+// 	modifiers: {
+// 		y: function (n) {
+// 			n = parseInt(n);
+// 			progressValueId.textContent = n.toFixed(0) + "%";
+
+// 		}
+// 	}
+// });
+
+
+// анимация прогрес бара - 2-й вариант
+
+
+function progressBarCircleAnimate(elementCircle = ".circular-progress-1", elementValue = "#progress-value-id1", percentCircle = 76, numberValue = 76 + "%", color = "rgba(248, 152, 40, 1)") {
+	gsap.fromTo(elementCircle, { background: `conic-gradient(${color} 0deg, #ededed 0deg)` }, {
+		scrollTrigger: {
+			trigger: elementCircle,
+			start: "bottom bottom",
+		},
+		background: `conic-gradient(${color} ${percentCircle * 3.6}deg, #ededed 0deg)`,
+		duration: 2,
+	});
+	//вариант анимации цифр
+	gsap.fromTo(elementValue,
+		{
+			textContent: 0,
+			color: color,
+		}, {
+		color: color,
+		scrollTrigger: {
+			trigger: elementCircle,
+			start: "bottom bottom",
+		},
+		textContent: numberValue,
+		duration: 2,
+		snap: { textContent: 1 }
+	});
+}
+progressBarCircleAnimate()
+progressBarCircleAnimate(".circular-progress-2", "#progress-value-id2", 90, 20, "rgba(245, 47, 110, 1)")
+progressBarCircleAnimate(".circular-progress-3", "#progress-value-id3", 85, 9452, "rgba(90, 135, 252, 1)")
+progressBarCircleAnimate(".circular-progress-4", "#progress-value-id4", 100, "100%", "rgba(3, 206, 164, 1)")
+
+
+
+//----------------------------------------------------------------------------------------
+//анимация прогрес бара через setInterval
 function progressBarAndNumber(the100percent = 100, endValue = 92, speed = 10, startValue = 0, objectProgressClass = ".circular-progress", objectValueClass = ".progress-value") {
 
 	const objectProgress = document.querySelector(objectProgressClass)
 	const objectValue = document.querySelector(objectValueClass)
 
 	// let startValue = 0,
-		// endValue = 92,
-		// speed = 10;
+	// endValue = 92,
+	// speed = 10;
 
 	let progress = setInterval(() => {
 		startValue++;
 
-		objectValue.textContent = `${startValue}%`;
+		// objectValue.textContent = `${startValue}%`;
 
 		objectProgress.style.background = `conic-gradient(#7d2ae8 ${startValue * 3.6}deg, #ededed 0deg)`;
 		//3.6deg * 100 = 360deg
@@ -29,4 +93,5 @@ function progressBarAndNumber(the100percent = 100, endValue = 92, speed = 10, st
 	}, speed);
 }
 
-progressBarAndNumber();
+// progressBarAndNumber();
+//----------------------------------------------------------------------------------------
